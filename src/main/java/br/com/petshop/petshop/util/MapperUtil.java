6a80990b90d1a145dcb13373da5300e0e48bebf9
Douglas Dreer <douglasdreer@outlook.com.br>
2023-06-29 11:41:11 -0300

    Melhorias diversas no arquivo de utilidade

--- src/main/java/br/com/petshop/petshop/util/MapperUtil.java
+++ src/main/java/br/com/petshop/petshop/util/MapperUtil.java
@@ -1,26 +1,50 @@
 package br.com.petshop.petshop.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Component
 public class MapperUtil {
     private static final ModelMapper modelMapper = new ModelMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+     MapperUtil() {
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
+        objectMapper.registerModule(javaTimeModule);
+    }
 
     public static <D> D convertTo(Object bean, Class<D> dto) {
         return bean == null ? null : modelMapper.map(bean, dto);
+    }
+
+    public static <D> D convertTo(String bean, Class<D> dto) {
+        return bean.isEmpty() ? null : modelMapper.map(bean, dto);
     }
 
     public static <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
         return source
                 .stream()
                 .map(element -> modelMapper.map(element, targetClass))
-                .collect(Collectors.toList());
+                .toList();
     }
 
-
+    public static String toJSON(Object item) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(item);
+    }
 }
